c9d099d5c1fd800ac4cc92a7fe3110094135b511
Rushil Karani <karanir@mcmaster.ca>
2025-01-22 15:09:59 -0500

    "walking skeleton" for player class

--- src/main/java/ca/mcmaster/se2aa4/mazerunner/Player.java
+++ src/main/java/ca/mcmaster/se2aa4/mazerunner/Player.java
@@ -1,5 +1,76 @@
 package ca.mcmaster.se2aa4.mazerunner;
 
 public class Player {
+
+    private int row;
+    private int col;
+    private String direction;
+
+    public Player(int startRow, int startCol) {
+        this.row = startRow;
+        this.col = startCol;
+        this.direction = "right";
+    }
+
+    public int getRow() {
+        return row;
+    }
+
+    public int getCol() {
+        return col;
+    }
+
+    public int[] getPosition(){
+        return new int[]{row, col};
+    }
+
+    public String getDirection() {
+        return direction;
+    }
+
+    public void moveForward() {
+        if (direction.equalsIgnoreCase("up")){
+            row--;
+        }
+        else if (direction.equalsIgnoreCase("down")){
+            row++;
+        }
+        else if (direction.equalsIgnoreCase("right")){
+            col++;
+        }
+        else{
+            col--;
+        }
+    }
+
+    public void turnLeft() {
+        if (direction.equalsIgnoreCase("up")){
+            direction="left";
+        }
+        else if (direction.equalsIgnoreCase("down")){
+            direction="right";
+        }
+        else if (direction.equalsIgnoreCase("right")){
+            direction="up";;
+        }
+        else{
+            direction="down";
+        }
+    }
+
+    public void turnRight() {
+        if (direction.equalsIgnoreCase("up")){
+            direction="right";
+        }
+        else if (direction.equalsIgnoreCase("down")){
+            direction="left";
+        }
+        else if (direction.equalsIgnoreCase("right")){
+            direction="down";;
+        }
+        else{
+            direction="up";
+        }
+    }
     
 }

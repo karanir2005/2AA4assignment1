1d516b4095c5325aefb9420602138c4c12c1fdc6
Rushil Karani <karanir@mcmaster.ca>
2025-01-22 23:34:08 -0500

    changes to main for "walking skeleton"

--- src/main/java/ca/mcmaster/se2aa4/mazerunner/Main.java
+++ src/main/java/ca/mcmaster/se2aa4/mazerunner/Main.java
@@ -1,15 +1,16 @@
 package ca.mcmaster.se2aa4.mazerunner;
 
 import org.apache.commons.cli.*;
-import java.io.BufferedReader;
+//import java.io.BufferedReader;
 //import java.io.File;
-import java.io.FileReader;
+import java.util.*;
+//import java.io.FileReader;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 public class Main {
 
-    private static final Logger logger = LogManager.getLogger();
+    private static Logger logger = LogManager.getLogger();
 
     public static void main(String[] args) {
         logger.info("** Starting Maze Runner");
@@ -21,35 +22,97 @@ public class Main {
             .desc("Path to the maze file")
             .build());
 
-        CommandLineParser parser = new DefaultParser();
-        CommandLine cmd;
-
+        options.addOption(Option.builder("p")
+            .longOpt("path")
+            .hasArg()
+            .desc("Path sequence to verify")
+            .build());
 
         try {
-            cmd = parser.parse(options, args);
-            String inputFilePath = cmd.getOptionValue("i");
+            CommandLineParser parser = new DefaultParser();
+            CommandLine cmd = parser.parse(options, args);
 
-            logger.info("**** Reading the maze from file " + inputFilePath);
-            BufferedReader reader = new BufferedReader(new FileReader(inputFilePath));
-            String line;
-            
-            while ((line = reader.readLine()) != null) {
-                StringBuilder output = new StringBuilder();
-                for (int idx = 0; idx < line.length(); idx++) {
-                    if (line.charAt(idx) == '#') {
-                        output.append("WALL ");
-                    } else if (line.charAt(idx) == ' ') {
-                        output.append("PASS ");
-                    }
-                }
-                logger.trace(output.toString());
+            String inputFilePath = cmd.getOptionValue("i");
+            String pathSequence = cmd.getOptionValue("p");
+
+            Maze maze = new Maze(inputFilePath);
+            Player player = new Player(maze.getEntryPoint()[0], maze.getEntryPoint()[1]);
+            Path path = new Path();
+
+            if (pathSequence != null) {
+                logger.info("Verifying path.");
+                boolean isValid = verifyPath(maze, player, pathSequence);
+                logger.info("Path validity: " + (isValid ? "VALID" : "INVALID"));
+            } else {
+                logger.info("Exploring maze and generating path...");
+                exploreMaze(maze, player, path);
+                logger.info("Canonical Path: " + path.getCanonicalPath());
+                logger.info("Factorized Path: " + path.getFactorizedPath());
             }
-            reader.close();
-        } catch(Exception e) {
+        } 
+        catch(Exception e) {
             logger.error("/!\\ An error has occured /!\\");
         }
         logger.info("**** Computing path");
         logger.warn("PATH NOT COMPUTED");
         logger.info("** End of MazeRunner");
     }
+
+
+
+    private static void exploreMaze(Maze maze, Player player, Path path) {
+        //process user inputs here
+        //take user input and move the player on screen
+        Scanner inp = new Scanner(System.in);
+        String move;
+        while (true){
+            logger.info("Enter Move (q to quit): ");
+            move = inp.nextLine();
+            if (move.equalsIgnoreCase("F")){
+                player.moveForward();
+            }
+            else if (move.equalsIgnoreCase("R")){
+                player.turnRight();
+            }
+            else if (move.equalsIgnoreCase("L")){
+                player.turnLeft();
+            }
+            else if (move.equalsIgnoreCase("q")){
+                break;
+            }
+            else{
+                logger.info("invalid input");
+            }
+        }
+        inp.close();
+    }
+    
+    private static boolean verifyPath(Maze maze, Player player, String pathSequence) {
+        String path = pathSequence.replace(" ", "");
+
+        //uses REGEX to check if all characters in path are valid
+        if (!path.matches("[FRL]*")) {
+            return false;
+        }
+        
+        for (char move : path.toCharArray()) {
+            if (move=='F'){
+                player.moveForward();
+            }
+            else if (move=='R'){
+                player.turnRight();
+            }
+            else if (move=='L'){
+                player.turnLeft();
+            }
+            
+            if (maze.isWall(player.getRow(), player.getCol())) {
+                return false;
+            }
+        }
+
+        boolean atEndPoint = player.getRow() == maze.getExitPoint()[0] && player.getCol() == maze.getExitPoint()[1];
+        return atEndPoint;
+    }
+
 }

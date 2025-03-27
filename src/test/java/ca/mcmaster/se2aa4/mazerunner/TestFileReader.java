bdca2259ee517f6e75b67697eb6468c584d71ccd
Rushil Karani <karanir@mcmaster.ca>
2025-03-27 14:12:12 -0400

    Update TestFileReader.java

--- src/test/java/ca/mcmaster/se2aa4/mazerunner/TestFileReader.java
+++ src/test/java/ca/mcmaster/se2aa4/mazerunner/TestFileReader.java
@@ -11,32 +11,23 @@ import org.junit.jupiter.api.Test;
 public class TestFileReader {
 
     @Test
-    void testReadFileInvalidCharacter() {
+    void testReadFileInvalidFiles() {
         Exception exception = assertThrows(IllegalArgumentException.class, () -> {
             new Maze("examples/invalid.maz.txt"); // Assume file has characters other than '#' and ' '
         });
         assertEquals("Error: Maze contains invalid characters. Only '#' and ' ' are allowed.", exception.getMessage());
-    }
 
-    @Test
-    void testReadFileInvalidPath() {
-        Exception exception = assertThrows(FileNotFoundException.class, () -> {
+        exception = assertThrows(FileNotFoundException.class, () -> {
             new Maze("filedne.txt"); // Assume file does not exist
         });
         assertEquals("Error: Maze file not found. Please enter valid path.", exception.getMessage());
-    }
 
-    @Test
-    void testReadFileEmptyMaze() {
-        Exception exception = assertThrows(IllegalArgumentException.class, () -> {
+        exception = assertThrows(IllegalArgumentException.class, () -> {
             new Maze("examples/empty.txt"); // Assume empty.txt is empty
         });
         assertEquals("Error: Maze file is empty.", exception.getMessage());
-    }
 
-    @Test
-    void testReadFileNoExitMaze() {
-        Exception exception = assertThrows(IllegalArgumentException.class, () -> {
+        exception = assertThrows(IllegalArgumentException.class, () -> {
             new Maze("examples/noEntry.maz.txt"); // Assume file has no entry/exit
         });
         assertEquals("Error: Maze must have a valid entry and exit point.", exception.getMessage());
